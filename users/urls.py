d1070b8b57ff4c03988185532dc6cd9ab1ffc73e
Den4200 <dpham.42@hotmail.com>
2020-06-03 17:26:42 -0400

    add logout path

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from users.views import LoginView, RegisterView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]

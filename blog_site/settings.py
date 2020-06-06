e9434c95d9da322f6de14c623f854b03ed8e8e3e
Den4200 <dennis@f1recloud.com>
2020-06-06 02:49:38 +0000

    prepare for production

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -15,7 +15,7 @@ import environ
 
 
 env = environ.Env(
-    DEBUG=(bool, True)
+    DEBUG=(bool, False)
 )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'f1recloud.com']
 
 
 # Application definition

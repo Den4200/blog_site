44e8a4bb5afe311670f069d34578de072c9ad3d8
Den4200 <dennis@f1recloud.com>
2020-06-07 01:09:35 +0000

    add www.f1recloud.com to ALLOWED_HOSTS

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'f1recloud.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'f1recloud.com', 'www.f1recloud.com']
 
 
 # Application definition

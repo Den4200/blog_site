5dab175d8117982c3d66580a49ffe269e99c6b54
Den4200 <dennis@f1recloud.com>
2020-06-07 22:12:00 +0000

    implement password reset

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordResetConfirmView, PasswordResetCompleteView, PasswordResetDoneView, PasswordResetView
 from django.urls import path
 
 from users.views import LoginView, ProfileView, RegisterView, UpdateProfileView
@@ -8,6 +8,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/<int:user_id>/', ProfileView.as_view(), name='profile'),
     path('update_profile/', UpdateProfileView.as_view(), name='update_profile')
